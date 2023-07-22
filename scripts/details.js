9d830656935fc4119799050d6f5fa8082addc5ca
JulianBrunelli <Julianbrunelli@outlook.com>
2023-07-21 23:18:37 -0300

    vue

--- scripts/details.js
+++ scripts/details.js
@@ -1,42 +1,31 @@
-let cardsDetails = document.getElementById("cardsDetails")
-let events = []
-fetch("https://mindhub-xj03.onrender.com/api/amazing")
-    .then(resulte => resulte.json())
-    .then(datos => {
-        events = datos.events
-        let idDetails = location.search
-        let paramsDetails = new URLSearchParams(idDetails)
-        let objectEvent = paramsDetails.get('idEvent')
-        let eventDetails = events.find(event => event._id == objectEvent)
-        crearMaqueta(eventDetails, cardsDetails)
-    })
-    .catch(error => console.error(error))
+const { createApp } = Vue;
 
-function crearMaqueta(event, container) {
-    container.innerHTML = `<div
-    class="card card-details-js mb-3 mt-3 col-9 d-flex flex-column align-items-center flex-lg-row"
-    >
-    <img
-        src="${event.image}"
-        class="card-img-top image-details"
-        alt=""
-    />
-    <section>
-        <h5 class="card-title text-center mt-1 text-decoration-underline">
-        ${event.name}
-        </h5>
-        <p>Date: ${event.date}</p>
-        <p>
-        Description: ${event.description}
-        </p>
-        <p>Category: ${event.category}</p>
-        <p>Place: ${event.place}</p>
-        <p>Capacity: ${event.capacity}</p>
-        <p>Assistance: ${event.assistance}</p>
-        <p>Price: $ ${event.price} USD</p>
-    </section>
-    </div>`
-}
+createApp({
+    data() {
+        return {
+            events: [],
+            idDetails: "",
+            paramsDetails: "",
+            idParameters: "",
+            eventDetails: null
+        };
+    },
+    created() {
+        fetch("https://mindhub-xj03.onrender.com/api/amazing")
+            .then((data) => data.json())
+            .then((data) => {
+                this.events = data.events;
+                this.idDetails = location.search
+                this.paramsDetails = new URLSearchParams(this.idDetails)
+                this.idParameters = this.paramsDetails.get('idEvent')
+                this.eventDetails = this.events.find(event => event._id == this.idParameters)
+                console.log(this.eventDetails);
+            })
+            .catch((error) => console.error(error.message));
+    },
+    computed: {
+    },
+}).mount("#app");
 
 
 
--- scripts/stats.js
+++ scripts/stats.js
@@ -8,12 +8,21 @@ createApp({
         return {
             events: [],
             current: "",
-            arrayCategories: [],
-            categoriesChecked: [],
-            inputSearch: "",
-            filterCrossed: [],
+            arrayCategoriesPast: [],
+            arrayCategoriesUpComing: [],
             upComingEvents: [],
             pastEvents: [],
+            percentage: [],
+            revenues: "",
+            percentageMinorAssist: "",
+            percentageMinorAssistName: "",
+            percentageMajorAssist: "",
+            percentageMajorAssistName: "",
+            arrayOrder: [],
+            nameMajorCapacity: "",
+            majorCapacity: Number,
+            forCategoryPast: [],
+            forCategoryUpComing: [],
         };
     },
     created() {
@@ -21,18 +30,54 @@ createApp({
             .then((data) => data.json())
             .then((data) => {
                 this.events = data.events;
-                this.arrayCategories = [...new Set(this.events.map((event) => event.category))];
                 this.current = data.currentDate
                 this.upComingEvents = this.events.filter(event => event.date >= this.current)
                 this.pastEvents = this.events.filter(event => event.date < this.current)
+                this.arrayCategoriesPast = [...new Set(this.pastEvents.map((event) => event.category))];
+                this.arrayCategoriesUpComing = [...new Set(this.upComingEvents.map((event) => event.category))];
+                this.arrayOrder = this.events.sort((a, b) => a.capacity - b.capacity)
+                this.nameMajorCapacity = this.arrayOrder[this.arrayOrder.length - 1].name
+                this.majorCapacity = this.arrayOrder[this.arrayOrder.length - 1].capacity
+                this.percentage = this.pastEvents.sort((a, b) => this.calculatePercentage(a.assistance, a.capacity) - this.calculatePercentage(b.assistance, b.capacity))
+                this.percentageMinorAssist = this.calculatePercentage(this.percentage[0].assistance, this.percentage[0].capacity)
+                this.percentageMinorAssistName = this.percentage[0].name
+                this.percentageMajorAssist = this.calculatePercentage(this.percentage[this.percentage.length - 1].assistance, this.percentage[this.percentage.length - 1].capacity).toFixed(2)
+                this.percentageMajorAssistName = this.percentage[this.percentage.length - 1].name
+                this.forCategoryPast = this.arrayCategoriesPast.map(category => {
+                    let eventFilter = this.pastEvents.filter(event => event.category == category)
+                    assistance = 0;
+                    capacity = 0;
+                    let revenuesPastEvents = 0;
+                    let percentageOfAssistancePastEvents = 0;
+                    for (const event of eventFilter) {
+                        revenuesPastEvents += event.price * event.assistance
+                        assistance += event.assistance
+                        capacity += event.capacity
+                    }
+                    percentageOfAssistancePastEvents = this.calculatePercentage(assistance, capacity).toFixed()
+                    return { name: category, revenues: revenuesPastEvents.toLocaleString(), assistance: percentageOfAssistancePastEvents };
+                })
+                this.forCategoryUpComing = this.arrayCategoriesUpComing.map(category => {
+                    let eventFilter = this.upComingEvents.filter(event => event.category == category)
+                    estimate = 0;
+                    capacity = 0;
+                    let revenuesUpcoming = 0;
+                    let percentageOfAssistanceUpcoming = 0;
+                    for (const event of eventFilter) {
+                        revenuesUpcoming += event.price * event.estimate
+                        estimate += event.estimate
+                        capacity += event.capacity
+                    }
+                    percentageOfAssistanceUpcoming = this.calculatePercentage(estimate, capacity).toFixed()
+                    return { name: category, revenues: revenuesUpcoming.toLocaleString(), estimate: percentageOfAssistanceUpcoming };
+                })
             })
             .catch((error) => console.error(error.message));
     },
-    computed: {
-        filterValue() {
-            this.filterCrossed = this.pastEvents.filter(event => {
-                return event.name.toLowerCase().includes(this.inputSearch.toLowerCase()) && (this.categoriesChecked.includes(event.category) || this.categoriesChecked.length == 0)
-            })
+    methods: {
+        calculatePercentage(assistance, capacity) {
+            let porcentaje = (assistance / capacity) * 100
+            return porcentaje
         }
     },
 }).mount("#app");
